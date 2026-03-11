8c31c1010f857086cf25d533abc585ede898a829
Elliott R Palmer <95081682+Justelliott6@users.noreply.github.com>
2026-03-11 11:40:07 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("playlist-skins-cache").then(cache => {
+      return cache.addAll([
+        "/Playlist-Skins-App/index.html",
+        "/Playlist-Skins-App/boombox.png",
+        "/Playlist-Skins-App/manifest.json"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
 
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
